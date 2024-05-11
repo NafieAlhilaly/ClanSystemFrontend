f00d7468b080ac89a86145ea9ceeb0c14d34ca41
Nafie <warrior259@gmail.com>
2024-05-11 14:17:25 +0300

    chore: add event listener on user state to direct user to login page when not logged in

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
+import useLocalStorage from "@/store";
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const { getUserData } = useLocalStorage()
+  function checkUserData() {
+    if (!getUserData() || !getUserData().username) {
+      router.push("/login")
+    }
+  }
+  useEffect(() => {
+    checkUserData()
+    window.addEventListener('storage', checkUserData);
+    return () => {
+      window.removeEventListener('storage', checkUserData)
+    }
+  }, [])
   return <Component {...pageProps} />;
 }
